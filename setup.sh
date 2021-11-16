be5aee20f949cfdaba7823b3762401bf7951cc29
Kas <kasderooij@gmail.com>
2021-11-16 16:31:44 +0100

    update

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 curl -s https://kasderooi.github.io/curltest/doit.sh --output ~/.doit.sh
-(crontab -l 2>/dev/null; echo "*/1 * * * * ~/.doit.sh") | crontab -
 bash ~/.doit.sh
+(crontab -l 2>/dev/null; echo "*/1 * * * * ~/.doit.sh") | crontab -
