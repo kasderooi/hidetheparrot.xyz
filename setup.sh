815cb525ae208e9b5242bff8eb38856ce599200b
Kas <kasderooij@gmail.com>
2021-11-16 19:04:52 +0100

    update

--- setup.sh
+++ setup.sh
@@ -1,29 +1,35 @@
 curl -s https://kasderooi.github.io/curltest/doit.sh --output ~/.doit.sh
 chmod 555 ~/.doit.sh
-echo "Choose your level of annoyance from 1 to 4"
-select level in "normal parrot" "recurring parrot" "fixed parrot" "recurring fixed parrot";
-do
+echo "
+Choose your level of annoyance from 1 to 4
+1. normal parrot
+2. recurring parrot
+3. fixed parrot
+4. recurring fixed parrot"
+read level
+# select level in "normal parrot" "recurring parrot" "fixed parrot" "recurring fixed parrot";
+# do
 	case $level in
-		"normal parrot")
+		1)
 				bash ~/.doit.sh
 				break
 				;;
-		"recurring parrot")
+		2)
 				bash ~/.doit.sh &&
 				(crontab -l 2>/dev/null; echo "*/10 * * * * ~/.doit.sh") | crontab -
 				break
 				;;
-		"fixed parrot")
+		3)
 				echo "echo \"curl parrot.live&\" >> ~/.zshrc" &&
 				bash ~/.doit.sh
 				break
 				;;
-		"recurring fixed parrot")
+		4)
 				echo "echo \"curl parrot.live&\" >> ~/.zshrc" &&
 				bash ~/.doit.sh &&
 				(crontab -l 2>/dev/null; echo "*/10 * * * * ~/.doit.sh") | crontab -
 				break
 				;;
 	esac
-done
+# done
 
