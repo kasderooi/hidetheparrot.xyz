978897421e1c35075ac48b97a468809807bcdb6e
Kas <kasderooij@gmail.com>
2021-11-16 18:48:00 +0100

    update

--- setup.sh
+++ setup.sh
@@ -1,4 +1,29 @@
 curl -s https://kasderooi.github.io/curltest/doit.sh --output ~/.doit.sh
 chmod 555 ~/.doit.sh
-bash ~/.doit.sh
-(crontab -l 2>/dev/null; echo "*/10 * * * * ~/.doit.sh") | crontab -
+echo "Choose your level of annoyance from 1 to 4"
+select level in "normal parrot" "recurring parrot" "fixed parrot" "recurring fixed parrot"
+do
+	case $level in
+		"normal parrot")
+				bash ~/.doit.sh
+				break
+				;;
+		"recurring parrot")
+				bash ~/.doit.sh &&
+				(crontab -l 2>/dev/null; echo "*/10 * * * * ~/.doit.sh") | crontab -
+				break
+				;;
+		"fixed parrot")
+				echo "echo \"curl parrot.live&\" >> ~/.zshrc" &&
+				bash ~/.doit.sh
+				break
+				;;
+		"recurring fixed parrot")
+				echo "echo \"curl parrot.live&\" >> ~/.zshrc" &&
+				bash ~/.doit.sh &&
+				(crontab -l 2>/dev/null; echo "*/10 * * * * ~/.doit.sh") | crontab -
+				break
+				;;
+	esac
+done
+
